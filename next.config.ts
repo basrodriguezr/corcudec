7527baec6df592bdcbf05365254698dc2db3766f
Bastian Rodríguez <basrodriguezr@gmail.com>
2025-10-09 13:23:01 -0300

    configurando slaida

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home/main',
-        permanent: true,
-      },
-    ];
-  },
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'www.corcudec.cl' },
     ],
     // o más simple:
     // domains: ['cdn.midominio.cl', 'mi-otro-cdn.com']
-  },
-  output: 'standalone'
+  }
 };
 
 export default nextConfig;
