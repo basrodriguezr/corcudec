5aa1b61924074a929073026cb3441c177ebe5c24
Bastian Rodríguez <basrodriguezr@gmail.com>
2025-10-09 10:18:27 -0300

    se ajustan comillas

--- src/app/components/Multimedia.tsx
+++ src/app/components/Multimedia.tsx
@@ -6,13 +6,13 @@ export const Multimedia = () => {
       </div>
       <div className="texto-contenedor">
         <p>
-          &ldquo;Lorem ipsum dolor sit amet&rdquo;
+          "Lorem ipsum dolor sit amet";
           <br/><br/>
-          &ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
           et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
           cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.&rdquo;
+          culpa qui officia deserunt mollit anim id est laborum."
         </p>
       </div>
     </div>
