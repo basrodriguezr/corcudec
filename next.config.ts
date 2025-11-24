6f27ab06600fc190f263f7db6911c8b9052a11cd
Bastian Rodríguez <basrodriguezr@gmail.com>
2025-11-24 09:50:52 -0300

    ajuste en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,21 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "standalone",  
-//   images: {
-//     unoptimized: true,
-//     remotePatterns: [
-//       { protocol: 'https', hostname: 'www.corcudec.cl' },
-//       { protocol: 'https', hostname: 'media-scl2-1.cdn.whatsapp.net' },
-//       { protocol: 'https', hostname: 'pokeapi.co' },
-//       { protocol: 'https', hostname: 'raw.githubusercontent.com' }
-//     ],
-//     // o más simple:
-//     // domains: ['cdn.midominio.cl', 'mi-otro-cdn.com']
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  // basePath: "/corcudec",
   trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "www.corcudec.cl" },
-      { protocol: "https", hostname: "drupal.corcudec.cl", port: "", pathname: "/sites/default/files/**" },
+      {
+        protocol: "https",
+        hostname: "drupal.corcudec.cl",
+        port: "",
+        pathname: "/sites/default/files/**",
+      },
     ],
   },
 };
 
 export default nextConfig;
-
